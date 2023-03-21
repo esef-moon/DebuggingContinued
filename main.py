e549cc8623bcca973339c8bd1266f0fe9ea7de1a
Susi Franco <susi.franco32@gmail.com>
2023-03-21 11:37:36 -0700

    added a deepcopy of stage list so that the forloop did not kill the cells before counting neighbors for each row/colum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gameoflife import width, height, stage, print_stage, count_neighbors
 
+import copy
+
 def init_stage(stage):
     for v_pos in range(0, height):
       for h_pos in range(0, width):
@@ -14,18 +16,25 @@ def init_stage(stage):
 # 3. Any live cell with > 3 neighbors dies
 # 4. Any dead cell with 3 neighbors comes alive
 
+
 def one_generation(stage):
-    for v_pos in range(len(stage)):
-        for h_pos in range(len(v_pos)):
-            neighbors = count_neighbors(stage, v_pos, h_pos)
-            if not stage[v_pos][h_pos] and neighbors == 3:
+    stage_copy = copy.deepcopy(stage)
+   
+    for v_pos in range(len(stage_copy)):
+        for h_pos in range(len(stage_copy)):
+            neighbors = count_neighbors(stage_copy, v_pos, h_pos)
+   
+            if not stage_copy[v_pos][h_pos] and neighbors == 3:
                 stage[v_pos][h_pos] = True
-            elif stage[v_pos][h_pos] and neighbors < 2:
+            elif stage_copy[v_pos][h_pos] and neighbors < 2:
                 stage[v_pos][h_pos] = False
-            elif stage[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
+            elif stage_copy[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
                 stage[v_pos][h_pos] = True
-            elif stage[v_pos][h_pos] and neighbors > 3:
+            elif stage_copy[v_pos][h_pos] and neighbors > 3:
                 stage[v_pos][h_pos] = False
+    stage = stage_copy
+    
+
                 
 
 init_stage(stage)
